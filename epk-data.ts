8f537fc5f6a2eebf841d1c904dfe5f40a6bc2eb2
Eduardox3703 <eduardoxcelaya@gmail.com>
2025-06-27 15:39:01 -0600

    first commit

---
+++ epk-data.ts
@@ -0,0 +1,227 @@
+export const EPK_DATA = {
+    "audios-promocionales": [
+        {
+            title: "Desierto",
+            resources: [
+                {
+                    name: "DESIERTO_MSTR B.3.wav",
+                    link: "https://drive.google.com/file/d/14F7h29mCCsH8yL3GdEPSP8aJPA5ua3LX/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "Buzon",
+            resources: [
+                {
+                    name: "Fran - Buzon (Final Master) 48khz 24bit.wav",
+                    link: "https://drive.google.com/file/d/1AmStbHKqlOs0sZqPpCCOMOOorbdy4ZFD/view?usp=sharing",
+                },
+            ],
+        },
+        {
+            title: "DOLCE VITA",
+            resources: [
+                {
+                    name: "DOLCE VITA_MSTR Rev2.wav",
+                    link: "https://drive.google.com/file/d/1hbJDyQiRPRVGTK3qpY9NZL5LejFOQxTS/view?usp=sharing",
+                },
+            ],
+        },
+    ],
+    "fotos-oficiales": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "FRAN FOTO 01.jpg",
+                    link: "https://drive.google.com/file/d/1qCuWUBT6ZnpJC0ojSoyJ2RbPoEweaKId/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 02.jpg",
+                    link: "https://drive.google.com/file/d/1hMj2DICtX4N0adLJcUDwaHFeK4Mvs0Lg/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 03.jpg",
+                    link: "https://drive.google.com/file/d/1thCeYBB08xmdMjzYyiP6zUrfdj6qaLlu/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 04.jpg",
+                    link: "https://drive.google.com/file/d/1PmBAb35o0aFnUQpuFzrU193WVuAOYJH0/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 05.jpg",
+                    link: "https://drive.google.com/file/d/1ivON1bXj0NiWMZC09MXgbRZ6beI0I0iJ/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 06.jpg",
+                    link: "https://drive.google.com/file/d/19P6IjD4TpSTeABDvjhNoBZjq5Gj1MVte/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 07.jpg",
+                    link: "https://drive.google.com/file/d/1JL0nC8eHuJmuLnZbJ4SW7pvioO6qB3-Z/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 08.jpg",
+                    link: "https://drive.google.com/file/d/1JrLhpeu2yeNXN9LwZt7ISyoJ28OHdmOl/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 09.jpg",
+                    link: "https://drive.google.com/file/d/1k2Xa9GB1WfTqk45LTd9GUG7Euujbh4_a/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 10.jpg",
+                    link: "https://drive.google.com/file/d/1_6allrfsTjPxgEq5w7RCgOdHE7DAratg/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 11.jpg",
+                    link: "https://drive.google.com/file/d/1aJNWAi8Z85nZM6I0JBhJj7iBkT_TTHLN/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 12.jpg",
+                    link: "https://drive.google.com/file/d/1Zsu_lzx0qyGC6-BMquEKiKh3eIyL9L2O/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 13.jpg",
+                    link: "https://drive.google.com/file/d/1hGPi1J_PkPBGsmnXILtlmbmGsoXYOBXP/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 14.jpg",
+                    link: "https://drive.google.com/file/d/1SaafuDi9cK8r6Dtqq-L9bO9b5OUhAB0y/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 15.jpg",
+                    link: "https://drive.google.com/file/d/1IgQralbLkU1EMYnkrVDZ46L7DUi9arWJ/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 16.jpg",
+                    link: "https://drive.google.com/file/d/1ewkDLHjbgRKpfo3sobwVaWecMcqTn2Eq/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 17.jpg",
+                    link: "https://drive.google.com/file/d/1xi5E76HWKVDefT8HIPcXq5CkdUccG4ED/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 18.jpg",
+                    link: "https://drive.google.com/file/d/1emmia_81hxcPyh6ZU0ENh-FcgD-NR8ha/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 19.jpg",
+                    link: "https://drive.google.com/file/d/1Jr7Ct7ApKNePTpKkFWm2T9o2yTwgefRL/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 20.jpg",
+                    link: "https://drive.google.com/file/d/1OkK1T819dtcWnSJdnpOa11Qg6cvjjnSO/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 21.jpg",
+                    link: "https://drive.google.com/file/d/1TQgcthKEdUxSzhYeJycZuOrIpXEF7BFX/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 22.jpg",
+                    link: "https://drive.google.com/file/d/1H-GtWdPkffvICPmu-26Uc-gBXvY4gjs8/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 23.jpg",
+                    link: "https://drive.google.com/file/d/1KATcNRm9R89j9tv_GMe1auns9-6g-4jw/view?usp=sharing"
+                },
+                {
+                    name: "FRAN FOTO 24.jpg",
+                    link: "https://drive.google.com/file/d/1GnLCwfT9cCSh1ZTnZvHAQ5VUo_Dpe_8G/view?usp=sharing"
+                },
+            ],
+        },
+    ],
+    "logotipo-oficial": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "logo.png",
+                    link: "https://drive.google.com/file/d/1_zkWwhvhBDDG3uC_6E0Ai3WX9ecU4R0B/view?usp=sharing",
+                },
+            ],
+        },
+    ],
+    "rider-tecnico": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "archivo-ejemplo.pdf",
+                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc"
+                }
+            ]
+        }
+    ],
+    "brochure": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "archivo-ejemplo.pdf",
+                    link: "https://9l6qnyfkes.ufs.sh/f/oCuvxwbj7yrx2q0R1Lun0Rje3hU5TWIDNfbVEsPL7aB98Xkc"
+                }
+            ]
+        }
+    ],
+    "biografia": [
+        {
+            title: null,
+            resources: [
+                {
+                    name: "Biografía FRAN.jpg",
+                    link: "https://drive.google.com/file/d/1Wo43Gk6NgLs4-NWzA-sruEWuvVKmC_C_/view?usp=sharing",
+                },
+            ],
+        },
+    ],
+    "videos-oficiales":[
+        {
+            title: "Desierto",
+            resources: [
+                {
+                    name: "Fran_Desierto_DetrásDeCámaras_CDT01.mov",
+                    link: "https://drive.google.com/file/d/1V8qelY0fNH12NzKOWtuQnbadAmulAeL2/view?usp=sharing",
+                },
+                {
+                    name: "Desierto - Fran - Final 4K.mp4",
+                    link: "https://drive.google.com/file/d/16U6BZWbUpgL38HYlDAJ8m_mN46o_cngO/view?usp=sharing",
+                }
+            ],
+        }, 
+        {
+            title: "Buzon",
+            resources: [
+                {
+                    name: "Fran - Buzon - Video ortada.mp4",
+                    link: "https://drive.google.com/file/d/1tO0zShJg7T46dCygB28Ou5DAmKsvxA7H/view?usp=sharing",
+                },
+                {
+                    name: "Fran - Buzón (Visualizer corregido)(Alta).mov",
+                    link: "https://drive.google.com/file/d/1xmpAcF1gURwphKIs_PZfcaEriKAOJyW6/view?usp=sharing",
+                }
+            ],
+        },
+        {
+            title: "Dolce Vita",
+            resources: [
+                {
+                    name: "Fran_DolceVita_VisualizerMaster_4K.mov",
+                    link: "https://drive.google.com/file/d/1RTRBWSWi3DDUrjRDsVqBEdkyXSLd2zDG/view?usp=sharing",
+                },
+            ],
+        },
+    ],
+};
+
+export const EPK_VIEWS_NAMES: Record<string, string> = {
+    "audios-promocionales": "Audios Promocionales",
+    "fotos-oficiales": "Fotos Oficiales",
+    "logotipo-oficial": "Logotipo Oficial",
+    "videos-oficiales": "Videos Oficiales",
+    // "temas-promocionales": "Temas Promocionales",
+    // "rider-tecnico": "Rider Técnico",
+    // "brochure": "Brochure",
+    "biografia": "Biografía",
+    // "otros-elementos": "Otros Elementos",
+};
